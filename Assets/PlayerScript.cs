558efecad13da90bb42b961c99dbf4d291b786bb
Peter Young <young40@gmail.com>
2017-06-05 14:30:18 +0800

    update

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+	public float speed;
+	public Text uiText;
+
+	private int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical   = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+		GetComponent<Rigidbody> ().AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Coin") {
+			other.gameObject.SetActive (false);
+
+			count++;
+
+			updateCount ();
+		}
+	}
+
+	void updateCount() {
+		uiText.text = "Test: " + count;
+	}
+}
